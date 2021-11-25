5b6e3ae7e3a1b77521169aa360fba1026f75f674
Fikayo <fikfikky@gmail.com>
2021-11-25 19:55:22 +0100

    Webshrinker works

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const axios = require("axios").default;
+const btoa = require("btoa");
 let bodyParser = require("body-parser");
+
+const ws_api_key = "Qj6yWmAjnrMpZNx7JRpE";
+const ws_api_secret = "R87VQOnuh47ITqZQdUuU";
+const ws_api = "https://api.webshrinker.com/categories/v3";
 
 let port = process.env.PORT || "5000";
 
@@ -11,6 +17,45 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.post("/querywebshrinker", async (req, res) => {
+
+    let website = req.body.website;
+
+    console.log(website);
+
+    let message;
+
+    if(!website){
+        message = "Enter a Valid URL e.g example.com";
+    }
+
+
+    const encoded_url = btoa(website);
+
+    console.log(encoded_url);
+
+    let result;
+
+    try {
+        const response = await axios.get(`${ws_api}/${encoded_url}?taxonomy=webshrinker`, {
+            auth : {
+                username: ws_api_key,
+                password: ws_api_secret
+            }
+        });
+
+        result = response.data.data;
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+        result = error;
+    }
+
+    
+    
+    res.send(result);
 });
 
 app.post("/sendinfo", (req, res) => {
